c6a2c1e9de94f4ae71871ed5c99f01c20f6037e0
정진혁 <wlsgur8724@naver.com>
2025-08-08 15:53:06 +0900

    Test

---
+++ src/main/java/org/example/Head01_JavaProgrammingStart/example1/Test1.java
@@ -0,0 +1,11 @@
+package org.example.Head01_JavaProgrammingStart.example1;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+
+        int score = 88;
+        String result = (score >= 90) ? "a학점" : "B학점";
+        System.out.println("성적: " + result);
+    }
+}
---
+++ src/main/java/org/example/Head01_JavaProgrammingStart/example4/ForLoopExample.java
@@ -0,0 +1,9 @@
+package org.example.Head01_JavaProgrammingStart.example4;
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
